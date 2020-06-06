df2cd52d867cf1162f84b9d2b86902c3548617d3
ubuvolt <ubuvolt@ubuvolt>
2020-06-06 20:14:21 +0100

    viviv2

--- resources/views/layout/head_login.blade.php
+++ resources/views/layout/head_login.blade.php
@@ -5,5 +5,6 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link href="{{ URL::asset('../resources/css/loginstyle.css') }}" rel="stylesheet">
+    
 </head>
 
